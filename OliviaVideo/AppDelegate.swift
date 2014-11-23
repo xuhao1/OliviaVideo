7412f9510997398eb6d6bf6f4b0d8b0f99edb905
xuhao <xuhao3e8@gmail.com>
2014-11-24 02:52:05 +0800

    Initial Commit

---
+++ OliviaVideo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  OliviaVideo
+//
+//  Created by xuhao on 11/24/14.
+//  Copyright (c) 2014 xuhao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
